2cf63ae59b25d60d5a14833a6debe08397a585b2
Armend Lesi <armend.lesi@gmail.com>
2021-10-13 20:34:22 +0200

    add languange

--- api_service/app.js
+++ api_service/app.js
@@ -17,9 +17,19 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
+let language;
+let customerNotFound;
+let productNotFound;
+let orderNotFound;
+let orderAlreadyCanceled;
+let orderSuccessfullyCanceled;
+let addRepair;
+
 
 function processDialogFlowRequest(req, res) {
     const parameters = req.body.queryResult.parameters;
+    language = req.body.queryResult.languageCode;
+    setLanguage()
     switch (req.body.queryResult.intent.displayName) {
         case 'product.order':
             return orderProduct(parameters.productId, parameters.customerId);
@@ -32,6 +42,24 @@ function processDialogFlowRequest(req, res) {
     }
 }
 
+function setLanguage() {
+    if (language === 'de') {
+        customerNotFound = 'Kunde nicht gefunden';
+        productNotFound = 'Produkt nicht gefunden';
+        orderNotFound = 'Bestellung nicht gefunden';
+        orderAlreadyCanceled = 'Bestellung ist bereits abgebrochen';
+        orderSuccessfullyCanceled = 'Bestellung erfolgreich abgebrochen';
+        addRepair = 'Produkt in Reparatur aufgenommen';
+    } else {
+        customerNotFound = 'customer not found';
+        productNotFound = 'product not found';
+        orderNotFound = 'order not found';
+        orderAlreadyCanceled = 'order already canceled';
+        orderSuccessfullyCanceled = 'order successfully canceled';
+        addRepair = 'product added to repair';
+    }
+}
+
 function generateOrderId(){
     orderCount++;
     return orderCount > 9 ? "O-00" + orderCount : "O-000" + orderCount;
@@ -41,8 +69,8 @@ function orderProduct(productId, customerId){
     if (!productId || !customerId) return;
 
     let currentCustomer = customers.find(customer => customer.customerId === customerId);
-    if (!currentCustomer) return createDialogFlowResponse("Customer not found")
-    if (!products.find(p => p.productId === productId)) return createDialogFlowResponse("Product not found")
+    if (!currentCustomer) return createDialogFlowResponse(customerNotFound)
+    if (!products.find(p => p.productId === productId)) return createDialogFlowResponse(productNotFound)
 
     const deliveryDate = new Date(Date.now());
     deliveryDate.setDate(deliveryDate.getDate() + 2)
@@ -55,32 +83,60 @@ function orderProduct(productId, customerId){
     }
 
     currentCustomer.orders.push(newOrder)
+    let response;
+    if (language === 'de') {
+        response = "Bestell ID: " + newOrder.orderId +
+            " Status: " + statusLanguage(newOrder.status) +
+            " Lieferdatum: " + newOrder.deliveryDate.toDateString();
+    } else {
+        response = "Order ID: " + newOrder.orderId +
+            " Status: " + newOrder.status +
+            " Delivery Date: " + newOrder.deliveryDate.toDateString();
+    }
 
-    const response = "Order ID: " + newOrder.orderId +
-        " Status: " + newOrder.status +
-        " Delivery Date: " + newOrder.deliveryDate.toDateString();
 
     return createDialogFlowResponse(response)
+}
+
+function statusLanguage(status) {
+        switch (status) {
+            case 'open':
+                return 'offen'
+            case 'sent':
+                return 'gesendet'
+            case 'delivered':
+                return 'ausgeliefert'
+            case 'canceled':
+                return 'abgebrochen'
+        }
 }
 
 function getOrderInfo(orderId, customerId) {
     if (!orderId || !customerId) return;
     let currentCustomer = customers.find(customer => customer.customerId === customerId);
-    if (!currentCustomer) return createDialogFlowResponse("Customer not found");
+    if (!currentCustomer) return createDialogFlowResponse(customerNotFound);
 
     const order = customers
                     .find(customer => customer.customerId === customerId)
                     .orders
                     .find(order => order.orderId === orderId)
 
-    if (!order) return createDialogFlowResponse("Order not found")
+    if (!order) return createDialogFlowResponse(orderNotFound)
 
     const product = products.find(product => product.productId === order.productId);
-    const response = "Order ID: " + order.orderId +
-        " Product: " + product.name +
-        " Product Detail: " + product.description +
-        " Status: " + order.status +
-        " Delivery Date: " + order.deliveryDate.toDateString();
+    let response
+
+    if (language === 'de') {
+        response = "Bestell ID: " + order.orderId +
+            " Produkt: " + product.name +
+            " Status: " + statusLanguage(order.status) +
+            " Lieferdatum: " + order.deliveryDate.toDateString();
+    } else {
+        response = "Order ID: " + order.orderId +
+            " Product: " + product.name +
+            " Status: " + order.status +
+            " Delivery Date: " + order.deliveryDate.toDateString();
+    }
 
     return createDialogFlowResponse(response)
 }
@@ -95,10 +151,10 @@ function cancelOrder(orderId, customerId) {
 
     let response;
     if (order.status === "canceled") {
-        response = "order already canceld"
+        response = orderAlreadyCanceled
     } else {
         order.status = "canceled";
-        response = "order successfully canceled";
+        response = orderSuccessfullyCanceled;
     }
     return createDialogFlowResponse(response);
 }
@@ -106,8 +162,8 @@ function cancelOrder(orderId, customerId) {
 function repairOrder(orderId, customerId) {
     if (!orderId || !customerId) return;
     let currentCustomer = customers.find(customer => customer.customerId === customerId);
-    if (!currentCustomer) return createDialogFlowResponse("Customer not found");
-    if (!currentCustomer.orders.find(o => o.orderId === orderId)) return createDialogFlowResponse("Order not found");
+    if (!currentCustomer) return createDialogFlowResponse(customerNotFound);
+    if (!currentCustomer.orders.find(o => o.orderId === orderId)) return createDialogFlowResponse(orderNotFound);
 
     if (currentCustomer.repaires) {
         currentCustomer.repaires.push({
@@ -123,7 +179,7 @@ function repairOrder(orderId, customerId) {
         ]
     }
 
-    return createDialogFlowResponse("add repair")
+    return createDialogFlowResponse(addRepair)
 }
 
 function createDialogFlowResponse(fulfillmentText) {
