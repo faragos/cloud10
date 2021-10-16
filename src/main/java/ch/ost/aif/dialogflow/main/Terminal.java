b6ef20c794974f3359c168987c6edaf5c99fd573
Marco Endres <endres.marco@gmail.com>
2021-10-16 17:42:44 +0200

    Add language Support

--- src/main/java/ch/ost/aif/dialogflow/main/Terminal.java
+++ src/main/java/ch/ost/aif/dialogflow/main/Terminal.java
@@ -11,6 +11,7 @@ public class Terminal {
 	public static void main(String[] args) {
 		BookManager bm = new BookManager();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String language = "en-US";
 		try {
 			System.out.println("                                        \n" +
 					"                   *****,.,*****        \n" +
@@ -23,8 +24,34 @@ public class Terminal {
 					" **          *****     ***,           **\n" +
 					" ,**                                *** \n" +
 					"    ********************************    \n");
-			System.out.println("Hallo, please enter a line for the client and confirm with enter, q for quit, c to check the phone book.");
+			System.out.println("Hello, please enter your language. Available options: de, en");
+			System.out.println("Hallo, bitte gib deine Sprache ein. Mögliche Optionen: de, en");
 			String line = "";
+			while (true) {
+				line = br.readLine();
+				if (line.equals("")) { //skip empty lines
+					continue;
+				}
+				if (line.equals("de")) { // quit the application
+					language = "de-DE";
+					break;
+				}
+				if (line.equals("en")) { // quit the application
+					language = "en-US";
+					break;
+				}
+			}
+			if(language.equals("en-US")) {
+				System.out.println("Welcome at Cloud 10.");
+				System.out.println("The store one above cloud 9");
+				System.out.println("Please describe your wish.");
+				System.out.println("You can quit with 'q'");
+			} else {
+				System.out.println("Hallo willkommen bei Cloud 10.");
+				System.out.println("Dem Store eins über der Wolke 9");
+				System.out.println("Bitte beschreiben sie Ihr anliegen.");
+				System.out.println("Du kannst diesen Chat mit 'q' verlassen");
+			}
 			while (true) {
 				line = br.readLine();
 				if (line.equals("")) { //skip empty lines
@@ -33,11 +60,7 @@ public class Terminal {
 				if (line.equals("q")) { // quit the application
 					break;
 				}
-				if (line.equals("c")) { // check the phonebook
-					System.out.println(bm.toString());
-					continue;
-				}
-				CustomRequestBuilder.detectIntentTexts("cloud10-espf", line, "abcde", "en-US", bm);
+				CustomRequestBuilder.detectIntentTexts("cloud10-espf", line, "abcde", language, bm);
 			}
 			System.out.println("Goodbye");
 		} catch (IOException e) {
